2f50738d11e5ab8ec6d5456bd052bb6ffc6ecf8b
Binli <denny41606@gmail.com>
2023-03-15 19:35:33 +0800

    Netlify6

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -11,8 +11,8 @@ const Homepage = () => {
   let [page, setPage] = useState(1);
   let [currentSearch, setCurrentSearch] = useState("");
   const auth = process.env.REACT_APP_SERVER_API;
-  const initialURL = "/curated?page=1&per_page=20";
-  let searchURL = `/search?query=${input}&per_page=15&per_page=1`;
+  const initialURL = "/v1/curated?page=1&per_page=20";
+  let searchURL = `/v1/search?query=${input}&per_page=15&per_page=1`;
 
   const search = async (url) => {
     //藉由axios去向pexels拿到領取圖片的連結"initialURL"，並且要附上金鑰
@@ -32,9 +32,9 @@ const Homepage = () => {
     let newURL;
     setPage(page + 1);
     if (currentSearch === "") {
-      newURL = `/curated?page=${page + 1}&per_page=15`;
+      newURL = `/v1/curated?page=${page + 1}&per_page=15`;
     } else {
-      newURL = `/search?query=${currentSearch}&per_page=15&per_page=1`;
+      newURL = `/v1/search?query=${currentSearch}&per_page=15&per_page=1`;
     }
     let result = await axios.get(newURL, {
       headers: {
