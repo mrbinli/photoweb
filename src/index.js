dc6b9411394d134e5662445d8faaa8727f4f017f
Binli <denny41606@gmail.com>
2023-03-15 21:59:53 +0800

    Netlify100

--- src/index.js
+++ src/index.js
@@ -4,8 +4,4 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //渲染出app.js，也就是其中的html內容
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
